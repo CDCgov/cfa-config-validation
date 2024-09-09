a9600c84ed6dad04021036b1734745bf60ce7f56
amondal2 <ab59@cdc.gov>
2024-09-09 19:53:19 +0000

    WIP -- add local schema, utils, and route logic to load schema safely

--- validation-api/api/app.py
+++ validation-api/api/app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+import json 
+import os
+from flask import Flask, Response
+from http import HTTPStatus
+from api.utils import CONSTANTS
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def base():
+    """Root server route to ensure proper startup.
+    Returns:
+        str: Startup message.
+    """
+    return "<p>Config validation server running.</p>"
+
+@app.post("/validate")
+def validate():
+    """Server route to validate a user-supplied JSON configuration.
+    The route accepts a POST request with the configuration in the 
+    request body. Then, it conducts logic to:
+    1. Load the schema (locally or from Azure storage)
+    2. Validate the configuration against the schema,
+    3. Return the configuration if it's valid, otherwise raise an HTTPError
+
+    Returns:
+        flask.Response: response object with valid JSON or error message.
+    """
+    
+    # Load schema; locally for now. Return early if loading fails
+    try:
+        schema_path = os.path.join(CONSTANTS.get("base_dir", ""), CONSTANTS.get("local_schema_path", ""))
+        with open(schema_path) as fp:
+            schema = json.load(fp)
+    except (json.decoder.JSONDecodeError, FileNotFoundError) as err:
+        return Response(response=str(err), status=HTTPStatus.BAD_REQUEST)
+
+    
+    return schema
+
---
+++ validation-api/api/utils.py
@@ -0,0 +1,29 @@
+import os
+
+CONSTANTS = {
+     "base_dir": os.path.abspath(os.path.dirname(__file__)),
+     "local_schema_path": "local_schema.json"
+}
+
+def load_schema(local=True):
+     """Load schema to compare configuration against.
+
+    This function loads a schema to compare the user-
+    provided configuration object against. Currently,
+    it pulls from a local file stored in data/, but
+    future implementations will pull from Azure blob
+    storage so that it can be more easily maintained.
+
+    Args:
+        local (bool): Determines whether to use local
+        schema file or pull latest from Azure (currently
+        not implemented).
+
+    Returns:
+        dict: Loaded schema dictionary.
+
+    Raises:
+        json.decoder.JSONDecodeError: If the schema JSON is invalid.
+    """
+     
+     pass
