ca585d6829a7861fbcec875f5ec73d34681aea15
Agastya Mondal <ab59@cdc.gov>
2024-09-10 08:36:53 -0700

    Server setup and validation routes (#5)
    * WIP -- add local schema, utils, and route logic to load schema safely
    
    * add validation logic + error handling
    
    * move schema loading logic to utils
    
    * update requirements.txt
    
    * black formatting
    
    * black formatting
    
    * isort imports
    
    * isort imports
    
    * isort imports

--- validation-api/api/app.py
+++ validation-api/api/app.py
@@ -1,7 +1,51 @@
-from flask import Flask
+import json
+from http import HTTPStatus
+
+from api.utils import load_schema
+from flask import Flask, Response, request
+from jsonschema import validate
+from jsonschema.exceptions import SchemaError, ValidationError
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def base():
+    """Root server route to ensure proper startup.
+    Returns:
+        str: Startup message.
+    """
+    return "<p>Config validation server running.</p>"
+
+
+@app.post("/validate")
+def validate_config():
+    """Server route to validate a user-supplied JSON configuration.
+    The route accepts a POST request with the configuration in the
+    request body. Then, it conducts logic to:
+    1. Load the schema (locally or from Azure storage)
+    2. Validate the configuration against the schema,
+    3. Return the configuration if it's valid, otherwise raise an HTTPError
+
+    Returns:
+        flask.Response: response object with valid JSON or error message.
+    """
+
+    # Load schema; locally for now. Return early if loading fails
+    try:
+        schema = load_schema(local=True)
+    except (json.decoder.JSONDecodeError, FileNotFoundError) as err:
+        return Response(response=str(err), status=HTTPStatus.BAD_REQUEST)
+
+    # Validate configuration in request body against schema
+    request_json = request.get_json(silent=False)
+    try:
+        validate(request_json, schema)
+    except (ValidationError, SchemaError) as err:
+        return Response(response=str(err), status=HTTPStatus.BAD_REQUEST)
+
+    return request_json, HTTPStatus.OK
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ validation-api/api/utils.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+CONSTANTS = {
+    "base_dir": os.path.abspath(os.path.dirname(__file__)),
+    "local_schema_path": "local_schema.json",
+}
+
+
+def load_schema(local=True):
+    """Load schema to compare configuration against.
+
+    This function loads a schema to compare the user-
+    provided configuration object against. Currently,
+    it pulls from a local file stored in data/, but
+    future implementations will pull from Azure blob
+    storage so that it can be more easily maintained.
+
+    Args:
+        local (bool): Determines whether to use local
+        schema file or pull latest from Azure (currently
+        not implemented).
+
+    Returns:
+        dict: Loaded schema dictionary.
+
+    Raises:
+        json.decoder.JSONDecodeError: If the schema JSON is invalid.
+    """
+    schema_path = os.path.join(
+        CONSTANTS.get("base_dir", ""), CONSTANTS.get("local_schema_path", "")
+    )
+    with open(schema_path) as fp:
+        schema = json.load(fp)
+    return schema
