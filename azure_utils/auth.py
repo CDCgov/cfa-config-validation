b1a159424867e38918be75e2a5293b847a0192ad
Agastya Mondal <ab59@cdc.gov>
2024-09-24 10:54:09 -0700

    remove explicit env variable check now that Managed Identity is configured (#21)

--- azure_utils/auth.py
+++ azure_utils/auth.py
@@ -1,5 +1,3 @@
-import os
-
 import toml
 from azure.identity import DefaultAzureCredential
 
@@ -19,20 +17,12 @@ def obtain_sp_credential():
     """Obtains service principal credentials from Azure.
     Returns:
         Instance of DefaultAzureCredential.
+    Raises:
+        LookupError if credential not found.
     """
 
-    # Since this will be run from a Docker container,
-    # we only use the SP Credential to authenticate.
-
-    # Check that env variables are set
-    tenant_id = os.environ.get("AZURE_TENANT_ID", "")
-    application_id = os.environ.get("AZURE_CLIENT_ID", "")
-    client_secret = os.environ.get("AZURE_CLIENT_SECRET", "")
-    if "" in [tenant_id, client_secret, application_id]:
-        error_message = "Azure secrets not found in environment. Ensure secrets are configured properly."
-        raise ValueError(error_message)
-
     # The DefaultAzureCredential reads from the environment directly
+    # if running locally. The deployed version uses Managed Identity
     sp_credential = DefaultAzureCredential()
 
     return sp_credential
